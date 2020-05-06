a8f3a20ae4f8001175081818f36ea0c5a34409bc
xraplol313 <64854124+xraplol313@users.noreply.github.com>
2020-05-06 19:52:23 +0300

    start

---
+++ Js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slider-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
+        prevArrow: '<button class="slider-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>'
+    });
+});
+    
