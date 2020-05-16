4bfd00ad1474fb04ebfde6eaa5148b5a7d448817
xraplol313 <64854124+xraplol313@users.noreply.github.com>
2020-05-16 13:32:51 +0300

    Menu ready

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,16 @@
-$(function(){
+$(function () {
     $('.slider').slick({
         arrows: false,
         fade: true,
         autoplay: 3000,
         dots: true
     });
+
+    $('.header-btn').on('click', function (){
+        $('.menu').addClass('active');
+    });
+    $('.close-btn').on('click', function (){
+        $('.menu').removeClass('active');
+    })
 });
     
